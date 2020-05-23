a1c5c26819ea0b21663825e7d2b7616495e9e89a
SE <saidelshibiny@Saids-MacBook-Pro.local>
2020-05-23 17:29:19 -0400

    pushing game to git repo

---
+++ Assets/Scripts/Ammo.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Ammo : MonoBehaviour {
+
+    //Variables
+    [SerializeField]
+    GameUI gameUI;
+    [SerializeField]
+    private int pistolAmmo = 20;
+    [SerializeField]
+    private int shotgunAmmo = 10;
+    [SerializeField]
+    private int assaultRifleAmmo = 50;
+    public Dictionary<string, int> tagToAmmo;
+
+    void Awake()
+    {
+        tagToAmmo = new Dictionary<string, int>
+        {
+            { Constants.Pistol , pistolAmmo },
+            { Constants.Shotgun , shotgunAmmo },
+            { Constants.AssaultRifle , assaultRifleAmmo },
+        };
+    }
+
+    //add ammo to the gun
+    public void AddAmmo(string tag, int ammo)
+    {
+        if (!tagToAmmo.ContainsKey(tag))
+        {
+            Debug.LogError("Unrecognized gun type passed: " + tag);
+        }
+        tagToAmmo[tag] += ammo;
+    }
+
+    // Returns true if gun has ammo
+    public bool HasAmmo(string tag)
+    {
+        if (!tagToAmmo.ContainsKey(tag))
+        {
+            Debug.LogError("Unrecognized gun type passed: " + tag);
+        }
+        return tagToAmmo[tag] > 0;
+    }
+
+    //return the bullet count
+    public int GetAmmo(string tag)
+    {
+        if (!tagToAmmo.ContainsKey(tag))
+        {
+            Debug.LogError("Unrecognized gun type passed:" + tag);
+        }
+        return tagToAmmo[tag];
+    }
+    
+    //consume ammunition
+    public void ConsumeAmmo(string tag)
+    {
+        if (!tagToAmmo.ContainsKey(tag))
+        {
+            Debug.LogError("Unrecognized gun type passed:" + tag);
+        }
+        tagToAmmo[tag]--;
+
+        gameUI.SetAmmoText(tagToAmmo[tag]);
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/GunEquipper.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunEquipper : MonoBehaviour {
+
+    //Reference to the
+    [SerializeField]
+    GameUI gameUI;
+
+    [SerializeField]
+    Ammo ammo;
+
+
+    //Variables
+    public static string activeWeaponType;
+    public GameObject pistol;
+    public GameObject assaultRifle;
+    public GameObject shotgun;
+    //reference to each gun |  activeGun is the currently equipped gun
+    GameObject activeGun;
+
+    // Use this for initialization
+    void Start()
+    {
+        //init the starting gun as a pistol
+        activeWeaponType = Constants.Pistol;
+        activeGun = pistol;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            loadWeapon(pistol);
+            activeWeaponType = Constants.Pistol;
+            gameUI.UpdateReticle();
+        }
+        else if (Input.GetKeyDown("2")){ 
+            loadWeapon(assaultRifle);
+            activeWeaponType = Constants.AssaultRifle;
+            gameUI.UpdateReticle();
+        }
+        else if (Input.GetKeyDown("3"))
+        {
+            loadWeapon(shotgun);
+            activeWeaponType = Constants.Shotgun;
+            gameUI.UpdateReticle();
+        }
+    }
+
+    //load gun method | method will turn off the all gun gameobjects and set the passed game object as active
+    private void loadWeapon(GameObject weapon)
+    {
+        pistol.SetActive(false);
+        assaultRifle.SetActive(false);
+        shotgun.SetActive(false);
+        weapon.SetActive(true);
+        activeGun = weapon;
+
+        gameUI.SetAmmoText(ammo.GetAmmo(activeGun.tag));
+    }
+
+    public GameObject GetActiveWeapon()
+    {
+        return activeGun;
+    }
+}
